e6d59bcc84b297911342a70951ed3fbcb8377624
JamesC133 <jamesc133@gmail.com>
2016-05-20 16:04:39 -0400

    Add files via upload

---
+++ JamesChristensenTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JamesChristensenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            SudokuPuzzleTests.SolveTest();
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ JamesChristensenTest/sudokuPuzzleTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace JamesChristensenTest
+{
+    class SudokuPuzzleTests
+    {
+     
+
+        public static void SolveTest()
+        {
+            //loop through the test files
+            for (int x = 1; x < 6; x++ )
+            {
+                var stream = new StreamReader("puzzle" + x.ToString() + ".txt");
+                var puzzles = new List<String>();
+                while (!stream.EndOfStream)
+                    puzzles.Add(stream.ReadLine());
+                stream.Close();
+                string fileName = "puzzle" + x.ToString() + "solution.txt";
+                
+                var times = puzzles.Select(p =>
+                {
+                    DateTime start = DateTime.Now;
+                    var puz = new SudokuPuzzle(p).Solve();
+                    SudokuPuzzle.Output(puz, fileName);
+                    return (DateTime.Now - start);
+                }).ToList();
+            }
+        }
+
+     
+    }
+}
